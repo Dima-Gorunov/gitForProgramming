e8947609731a5a6781dec70b0b6053e3cfc1e380
Dima-Gorunov <gorunov-01@mail.ru>
2022-10-23 06:27:53 +0300

    new commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿//file is changed
+
+#include <iostream>
+#include <cassert> 
+#include <math.h> 
+
+float resistace(float R1, float R2, float R3)
+{
+    return sqrt((R1 * R2 * R3) / (R1 * R2 + R2 * R3 + R3 * R1));  //поиск сопротив-я
+}
+
+int main()
+{
+
+    std::cout << resistace(2, 4, 1);
+        return 0;
+}
